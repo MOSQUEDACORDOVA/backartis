aba6e7390b47f91f88b33338d6cc79ef0b2489ce
josearzolay287 <84419079+josearzolay287@users.noreply.github.com>
2021-06-08 14:30:11 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
-sequelize = null;
-const DATABASE_URL= "https://data.heroku.com/datastores/a0f1f5e0-f1dc-46f6-86e1-c3d5703c351a"
-// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres'
-  })
-}
 
+// Variables desde variables.env
+require('dotenv').config({path: 'variables.env'});
+
+DB_NAME="back-artist";
+DB_USER="root";
+DB_PASS=null;
+DB_HOST="localhost";
+DB_PORT=3306;
+
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS,
+	{
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: 'mysql'
+	});
 
 module.exports = db;
