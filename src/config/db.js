e80a019547d84827428688b02c96b16f914be861
josearzolay287 <84419079+josearzolay287@users.noreply.github.com>
2021-06-08 14:51:33 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 // Variables desde variables.env
 //require('dotenv').config({path: 'variables.env'});
 
-DB_NAME="de69n41tmrgc7t";
-DB_USER="egpijhnywrkodb";
-DB_PASS="608bf8c663808da183e47320dfc52887d8756c2fde0276bf7ed37c941b525d4e";
-DB_HOST="ec2-34-193-101-0.compute-1.amazonaws.com";
+DB_NAME="heroku_d3ab0a1b9873300";
+DB_USER="bef9a651ee054d";
+DB_PASS="7139c364 ";
+DB_HOST="localhost";
 DB_PORT=5432;
 
 
@@ -14,7 +14,7 @@ const db = new Sequelize(DB_NAME, DB_USER, DB_PASS,
 	{
 		host: DB_HOST,
 		port: DB_PORT,
-		dialect: 'postgres'
+		dialect: 'mysql'
 	});
 
 module.exports = db;
