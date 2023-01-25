c4fed7ec2367ec987f3d450a7ced35b56ed03229
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2023-01-25 00:25:33 -0500

    CLIENT_ID,SECRET and access_token
    var CLIENT_ID =
      "";
    var SECRET =
      "";
    access_token:
            ""

--- src/controllers/mercadoPago.js
+++ src/controllers/mercadoPago.js
@@ -5,9 +5,9 @@ const BD_conect = require("../models/modulos_");
 var moment = require('moment-timezone');
 var request = require("request");
 var CLIENT_ID =
-  "AXAh-Ze4KzcjXTdOFXJ6U3ZagzU3aM8HUhRSI9Xn10Z8Ac2yXFQg58AbRzaGdfDS0achuY62g6sO_H9c";
+  "";
 var SECRET =
-  "ENE0CtubJ5-TSMh1TB9FN5kZW87G37C6tLi0c-c1hG-Zruz_yHKAOPEyllzTd-Lhzcl__PBllJ3-mGsk";
+  "";
 var PAYPAL_API = "https://api-m.paypal.com";
 
 exports.pasarela = async (req, res, next) => {
@@ -47,7 +47,7 @@ exports.pasarela = async (req, res, next) => {
     // Agrega credenciales
     mercadopago.configure({
       access_token:
-        "TEST-7704156678097466-010904-a33b280de26eb4d1e747cf84848e3706-696588363",
+        "",
     });
     console.log(modo + 'modo');
     // Crea un objeto de preferencia
